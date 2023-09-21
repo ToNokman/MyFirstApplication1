7b8ab4e9a9d70912c91502951e16916877f35399
TONOKMAN <1057218956@qq.com>
2023-09-21 15:04:11 +0800

    second commit

--- app/src/main/java/com/jnu/student/MainActivity2.java
+++ app/src/main/java/com/jnu/student/MainActivity2.java
@@ -1,14 +1,25 @@
 package com.jnu.student;
-
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.Color;
 import android.os.Bundle;
-
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 public class MainActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        RelativeLayout relativeLayout =  new RelativeLayout(this);
+        RelativeLayout.LayoutParams params =  new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,RelativeLayout.LayoutParams.WRAP_CONTENT);
+        //addRule 参数对应RelativeLayout XML 布局的属性
+        params.addRule(RelativeLayout.CENTER_IN_PARENT);     //设置居中
+        TextView textView = new TextView(this);     //创建TextView控件
+        textView.setText("HelloWorld");           //设置TextView的文字内容
+        textView.setTextColor(Color.RED);                   //设置TextView的文字颜色
+        textView.setTextSize(18);                           //设置TextView的文字大小
+        //添加TextView对象和TextView的布局属性
+        relativeLayout.addView(textView,params);
+        setContentView(relativeLayout);                     //设置在Activity中显示RelativeLayout
     }
 }
+
