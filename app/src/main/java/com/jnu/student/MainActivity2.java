8d9b14b0e2433defb3813bb7487f6662a70ed403
TONOKMAN <1057218956@qq.com>
2023-10-07 14:39:48 +0800

    third commit

--- app/src/main/java/com/jnu/student/MainActivity2.java
+++ app/src/main/java/com/jnu/student/MainActivity2.java
@@ -1,27 +1,45 @@
 package com.jnu.student;
-import androidx.appcompat.app.AppCompatActivity;
-import android.graphics.Color;
+
 import android.os.Bundle;
-import android.widget.RelativeLayout;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity2 extends AppCompatActivity {
+
+    private TextView textView1;
+    private TextView textView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        RelativeLayout relativeLayout =  new RelativeLayout(this);
-        RelativeLayout.LayoutParams params =  new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,RelativeLayout.LayoutParams.WRAP_CONTENT);
-        //addRule 参数对应RelativeLayout XML 布局的属性
-        params.addRule(RelativeLayout.CENTER_IN_PARENT);     //设置居中
-        TextView textView = new TextView(this);     //创建TextView控件
-        textView.setText(getString(R.string.tv_one));           //设置TextView的文字内容
-        textView.setTextColor(Color.RED);                   //设置TextView的文字颜色
-        textView.setTextSize(18);                           //设置TextView的文字大小
-        //添加TextView对象和TextView的布局属性
-        relativeLayout.addView(textView,params);
-        setContentView(relativeLayout);                     //设置在Activity中显示RelativeLayout
+        setContentView(R.layout.activity_main);
 
+        textView1 = findViewById(R.id.textView1);
+        textView2 = findViewById(R.id.textView2);
+        Button button = findViewById(R.id.button);
 
+        button.setOnClickListener(v -> {
+            // 获取两个TextView的文本
+            String text1 = textView1.getText().toString();
+            String text2 = textView2.getText().toString();
+
+            // 交换两个TextView的文本
+            textView1.setText(text2);
+            textView2.setText(text1);
+
+            // 显示交换成功的提示信息
+            Toast.makeText(MainActivity2.this, "交换成功", Toast.LENGTH_SHORT).show();
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity2.this);
+            builder.setMessage("交换成功")
+                    .setPositiveButton("确定", null)
+                    .show();
+        });
     }
 }
+
 
